e2f674fb3ea19c0baa1ed6f5930b3dab449d7718
zeVACa <logunik@bk.ru>
2021-03-21 11:10:15 +0300

    basic validation

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,13 +2,39 @@
 const User = require('../models/User');
 const Role = require('../models/Role');
 const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
 
 module.exports.register = async (req, res) => {
   try {
     // const { userName, password } = req.body;
-    const { userName, password } = req.body;
+    const validationErrors = validationResult(req);
 
-    console.log(userName, password);
+    if (!validationErrors.isEmpty()) {
+      res.status(400).json({ message: 'Ошибка при регистрации', errors: validationErrors });
+    }
+
+    const { username, password } = req.body;
+
+    const candidate = await User.findOne({ username });
+    // console.log('candidate is: ', candidate);
+
+    if (candidate) {
+      res.status(400).json({
+        message: 'user is already registered',
+        user: candidate,
+      });
+    }
+
+    // console.log('req data: ', userName, password);
+
+    const hashPassword = await bcrypt.hash(password, 7);
+    const userRole = await Role.findOne({ value: 'USER' });
+    const user = new User({ username: username, password: hashPassword, roles: [userRole.value] });
+    await user.save();
+    res.status(200).json({ mesage: 'user is registered sucessfully', user: user });
+
+    // const hashedPass = bcrypt.hash();
+
     // const candidate = await User.findOne({});
     // res.json({ test: 'Test' });
   } catch (e) {
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const controllerAuth = require('../controllers/auth');
+const { check } = require('express-validator');
 
-router.post('/register', controllerAuth.register);
+router.post(
+  '/register',
+  [
+    check('username', 'имя пользователя не может быть пустым').notEmpty(),
+    check('password', 'Пароль не может быть короче 8 символов и больше 20 символов').isLength({
+      min: 8,
+      max: 20,
+    }),
+  ],
+  controllerAuth.register,
+);
 router.post('/login', controllerAuth.login);
 router.get('/users', controllerAuth.getUsers);
 
